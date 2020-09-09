f0d192395a7675503c0ac68782363fd67a07e2b2
Josephshitandi <jsphshtnd@gmail.com>
2020-09-09 10:35:38 +0300

    Add a prototype function that finds service fee for heavy vehicle

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,22 @@ $(document).ready(function(){
         this.services = services;
         this.duration = duration;
     }
-    
-    
+    Booking.prototype.finalFee = function (){
+        return this.getServicesFee() + this.getDurationFee()
+    };
+    Booking.prototype.getServicesFee = function(){
+        if(this.type === "Heavy"){
+            if(this.services === "Engine service"){
+                return 5000;
+            }else if(this.services === "Power System"){
+                return 3000;
+            }else if(this.services === "Oil services"){
+                return 3500;
+            }else if(this.services === "Parking"){
+                return 600;
+            }else{
+                return 800;
+            }
+        }else 
+    }
 })

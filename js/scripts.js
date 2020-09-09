4293825ac6962783a9bbcdbb7db2831742769885
Josephshitandi <jsphshtnd@gmail.com>
2020-09-09 16:49:42 +0300

    Linked booking summary form with jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
-    function Booking(branch, type, services, duration) {
+    function Booking(name, email, phone, id, regNo, branch, type, services, duration) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.id = id;
+        this.regNo = regNo;
         this.branch = branch;
         this.type = type;
         this.services = services;
         this.duration = duration;
     }
+    var servicesOrdered = [];
     Booking.prototype.finalFee = function () {
         return this.getServicesFee() + this.getDurationFee()
     };
@@ -62,15 +68,78 @@ $(document).ready(function () {
             };
         };
     };
-    $("#userInput").submit(function(e){
+    $("#userInput").submit(function (e) {
         e.preventDefault();
+        var userName = $("#fname").val();
+        var userEmail = $("#email").val();
+        var userPhone = $("#fphone").val();
+        var userId = $("#fid").val();
+        var userRegNo = $("#fnumber").val();
         var userBranch = $("#branch").val();
         var userType = $("#type").val();
         var userService = $("#services").val();
         var userDuration = $("#duration").val();
-        var newBooking = new Booking(userBranch,userType,userService,userDuration);
+        var newBooking = new Booking(userName, userEmail, userPhone, userId, userRegNo, userBranch, userType, userService, userDuration)
         console.log(newBooking)
-        
+
+        servicesOrdered.push(newBooking);
+        $("#fname").val("");
+        $("#email").val("");
+        $("#fphone").val("");
+        $("#fid").val("");
+        $("#fnumber").val("");
+        $("#branch").val("");
+        $("#type").val("");
+        $("#services").val("");
+        $("#duration").val("");
+
+
+        $("#booking-summary").append(
+            "<tr>" +
+            '<th scope="row">' +
+            newBooking.name +
+            "</th>" +
+            "<td>" +
+            newBooking.email +
+            "</td>" +
+            "<td>" +
+            newBooking.phone +
+            "</td>" +
+            "<td>" +
+            newBooking.id +
+            "</td>" +
+            "<td>" +
+            newBooking.regNo +
+            "</td>" +
+            "<td>" +
+            newBooking.branch +
+            "</td>" +
+            "<td>" +
+            newBooking.type +
+            "</td>" +
+            "<td>" +
+            newBooking.services +
+            " - " +
+            newBooking.getServicesFee() +
+            "</td>" +
+            "<td>" +
+            newBooking.duration +
+            " - " +
+            newBooking.getDurationFee() +
+            "</td>" +
+            "<td>" +
+            newBooking.finalFee() +
+            "</td>" +
+            "</tr>"
+        );
+
+
+
 
     });
+
+
+
+
+
 });

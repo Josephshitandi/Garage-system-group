40f9af62f556c8857e66eb8663d3793db35bbcca
Josephshitandi <jsphshtnd@gmail.com>
2020-09-09 11:17:38 +0300

    Add a prototype function for finding duration fee

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,65 @@
-$(document).ready(function(){
-    function Booking(branch,type,services,duration){
+$(document).ready(function () {
+    function Booking(branch, type, services, duration) {
         this.branch = branch;
         this.type = type;
         this.services = services;
         this.duration = duration;
     }
-    Booking.prototype.finalFee = function (){
+    Booking.prototype.finalFee = function () {
         return this.getServicesFee() + this.getDurationFee()
     };
-    Booking.prototype.getServicesFee = function(){
-        if(this.type === "Heavy"){
-            if(this.services === "Engine service"){
-                return 5000;
-            }else if(this.services === "Power System"){
+    Booking.prototype.getServicesFee = function () {
+        if (this.type === "Medium") {
+            if (this.services === "Engine service") {
                 return 3000;
-            }else if(this.services === "Oil services"){
-                return 3500;
-            }else if(this.services === "Parking"){
-                return 600;
-            }else{
-                return 800;
-            }
-        }else if(this.type === "Medium"){
-            if(this.services === "Engine service"){
-                return 3000;
-            }else if(this.services === "Power System"){
+            } else if (this.services === "Power System") {
                 return 2500;
-            }else if(this.services === "Oil services"){
+            } else if (this.services === "Oil services") {
                 return 3000;
-            }else if(this.services === "Parking"){
+            } else if (this.services === "Parking") {
                 return 500;
-            }else{
+            } else {
                 return 600;
-            }
-        }else {
-            if(this.services === "Engine service"){
+            };
+        } else {
+            if (this.services === "Engine service") {
                 return 2000;
-            }else if(this.services === "Power System"){
+            } else if (this.services === "Power System") {
                 return 1500;
-            }else if(this.services === "Oil services"){
+            } else if (this.services === "Oil services") {
                 return 2500;
-            }else if(this.services === "Parking"){
+            } else if (this.services === "Parking") {
                 return 300;
-            }else{
+            } else {
                 return 300;
+            };
+        };
+    };
+    Booking.prototype.getDurationFee = function () {
+        if (this.type === "Heavy") {
+            if (this.duration === "Urgently") {
+                return 1000;
+            } else if (this.duration === "Late") {
+                return 3000;
+            } else {
+                return 0;
+            }
+        } else if (this.type === "Medium") {
+            if (this.duration === "Urgently") {
+                return 1000;
+            } else if (this.duration === "Late") {
+                return 1500;
+            } else {
+                return 0;
+            }
+        } else {
+            if (this.duration === "Urgently") {
+                return 800;
+            } else if (this.duration === "Late") {
+                return 1200;
+            } else {
+                return 0;
             }
         }
     }
-})
+});

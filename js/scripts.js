08ee515b35defb5bf43883061fb77e99d43e1dc5
Josephshitandi <jsphshtnd@gmail.com>
2020-09-09 09:55:44 +0300

    Add a constructor that will hold user input

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    function Booking(branch,type,services,duration){
+        this.branch = branch;
+        this.type = type;
+        this.services = services;
+        this.duration = duration;
+    }
+    
+    
+})
